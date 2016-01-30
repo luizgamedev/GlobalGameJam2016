78576fead651a98adc5e8bd28deae9c84883fdbc
Luiz Santos <santos.uff@gmail.com>
2016-01-30 20:40:54 +0000

    Fading Mechanic Menus Some screen Corrections

---
+++ GlobalGameJam2016/Assets/Scripts/OclusionAnimation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OclusionAnimation : MonoBehaviour {
+
+	Color opaqueBlack;
+	Color transparentBlack;
+	SpriteRenderer myRenderer;
+	float FadeSpeed;
+	float FadeOutSpeed;
+
+	// Use this for initialization
+	void Start () {
+		opaqueBlack = Color.black;
+		transparentBlack = Color.black;
+		transparentBlack.a = 0f;
+		myRenderer = GetComponent<SpriteRenderer> ();
+		FadeSpeed = 0.01f;
+		FadeOutSpeed = 0.02f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftControl) || Input.GetKey (KeyCode.RightControl)) {
+			myRenderer.color = Color.Lerp (myRenderer.color, opaqueBlack, FadeSpeed);
+		} else {
+			myRenderer.color = Color.Lerp (myRenderer.color, transparentBlack, FadeOutSpeed);
+		}
+	}
+}
