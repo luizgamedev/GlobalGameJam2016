f1007665af47f664cea77df5cb75a2a13d669457
Luiz Santos <santos.uff@gmail.com>
2016-01-30 13:42:38 +0000

    End Scene, Good and Bad Songs

--- GlobalGameJam2016/Assets/Scripts/StoryEndManager.cs
+++ GlobalGameJam2016/Assets/Scripts/StoryEndManager.cs
@@ -18,35 +18,44 @@ public class StoryEndManager : MonoBehaviour {
 		minimumFlowersForAHappyEnding = 10;
 		int flowerCount = 8;
 
-		if (GameManager.Instance) {
-			flowerCount = GameManager.Instance.GetFlowerCount ();
-		} else {
-			Debug.Log ("StoryEndManager -- No Manager!");
-			return;
-		}
+//		if (GameManager.Instance) {
+//			flowerCount = GameManager.Instance.GetFlowerCount ();
+//		} else {
+//			Debug.Log ("StoryEndManager -- No Manager!");
+//			return;
+//		}
 
+		musicSource = GameObject.FindGameObjectWithTag("Music");
+		AudioSource audioSource = musicSource.GetComponent<AudioSource> ();
+		if (audioSource.isPlaying) {
+			audioSource.Stop ();
+		}
 		if ( flowerCount < minimumFlowersForAHappyEnding) {
 			//Bad Ending!!!
 			goddessSpriteRenderer.color = Color.black;
+			if (musicSource) {
+
+
+				audioSource.clip = badEndingSong;
+				audioSource.Play ();			
+			}
 		} else {
 			//Happy Ending!!!
 			goddessSpriteRenderer.color = Color.cyan;
+			if (musicSource) {
+
+
+				audioSource.clip = goodEndingSong;
+				audioSource.Play ();			
+			}
 		}
 
 		//TODO: do a proper fading here!
 
 		//////////////////////////////////////////////////////////////////////////////////////////
 		//Change the main song
-		musicSource = GameObject.FindGameObjectWithTag("Music");
-		if (musicSource) {
-			AudioSource audioSource = musicSource.GetComponent<AudioSource> ();
-			if (audioSource.isPlaying) {
-				audioSource.Stop ();
-			}
 
-			audioSource.clip = goodEndingSong;
-			audioSource.Play ();			
-		}
+
 		//////////////////////////////////////////////////////////////////////////////////////////
 
 		Camera.main.GetComponent<Animator> ().SetTrigger ("StartCinematic");
