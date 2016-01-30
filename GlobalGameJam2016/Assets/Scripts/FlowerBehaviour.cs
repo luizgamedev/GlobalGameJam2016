74f165a162723b291a31613dad652f855ad05a8f
Luiz Santos <santos.uff@gmail.com>
2016-01-30 15:25:01 +0000

    Welcome to Lava!

--- GlobalGameJam2016/Assets/Scripts/FlowerBehaviour.cs
+++ GlobalGameJam2016/Assets/Scripts/FlowerBehaviour.cs
@@ -4,10 +4,19 @@ using System.Collections;
 public class FlowerBehaviour : MonoBehaviour {
 
 	public GameObject particlePrefab;
+	public Sprite[] spriteList;
+
+	SpriteRenderer myRenderer;
 
 	// Use this for initialization
 	void Start () {
-	
+		myRenderer = GetComponentInChildren<SpriteRenderer> ();
+
+		if(spriteList.GetLength(0) > 0){
+			
+			myRenderer.sprite = spriteList[Random.Range(0, spriteList.GetLength(0) - 1)];
+
+		}
 	}
 	
 	// Update is called once per frame
@@ -17,7 +26,9 @@ public class FlowerBehaviour : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D coll){
 		if(coll.tag == "Player"){
-			GameManager.Instance.AddFlowerCount ();
+			if (GameManager.Instance) {
+				GameManager.Instance.AddFlowerCount ();
+			}
 			GameObject particle = Instantiate (particlePrefab, transform.position, Quaternion.identity) as GameObject;
 			GameObject.Destroy (gameObject, 0.1f);
 			GameObject.Destroy (particle, 1f);
---
+++ GlobalGameJam2016/Assets/Scripts/LavaRandomAnimation.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LavaRandomAnimation : MonoBehaviour {
+
+	public int xBound;
+	public float maxVelocity;
+	bool isGoingRight;
+	float actualVelocity;
+	float index;
+
+	// Use this for initialization
+	void Start () {
+		maxVelocity = 8f;
+		actualVelocity = Random.Range (0.1f, maxVelocity);
+		isGoingRight = true;
+		xBound = 8;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		index += Time.deltaTime;
+
+		//Change Direction
+		if(transform.position.x > xBound){
+			transform.position = new Vector3 (xBound, transform.position.y, transform.position.z);
+			isGoingRight = false;
+			actualVelocity = -Random.Range (0.1f, maxVelocity);
+		}
+		if(transform.position.x < -xBound){
+			transform.position = new Vector3 (-xBound, transform.position.y, transform.position.z);
+			isGoingRight = true;
+			actualVelocity = Random.Range (0.1f, maxVelocity);
+		}
+
+//		//Randomly Change
+//		if(Random.Range(0,100) < 5){
+//			isGoingRight = !isGoingRight;
+//			actualVelocity = -actualVelocity;
+//		}
+
+		transform.Translate (new Vector3 ( (actualVelocity * Time.deltaTime), 0.01f*Mathf.Sin(15f*index), 0f  ));
+	}
+}
